a09e88ca58167d47f5f3f1037912c3ec4346b2b9
Leandro Valle <leandro_lefera@hotmail.com>
2023-01-26 08:53:41 -0300

    26/01/2023

--- GHSContratoWeb/Controllers/UnidadeConsumidoraController.cs
+++ GHSContratoWeb/Controllers/UnidadeConsumidoraController.cs
@@ -49,6 +49,76 @@ namespace GHSContratoWeb.Controllers
             EnderecoCliente endereco = new EnderecoClienteBusiness().Detalhes(id);
             return View(endereco);
         }
+
+        [HttpPost]
+        public ActionResult Cadastro(FormCollection form)
+        {
+            //EnderecoCliente unidadeConsumidora = new EnderecoCliente()
+            //{
+            //    ID = form["ID"].ToInt32(),
+            //    Nome = form["Nome"].IsNullOrEmptyBoolean() ? null : form["Nome"].Trim(),
+            //    Ativo = form["clienteAtivo"].ToBoolean(),
+            //    DataExpedicao = form["DataExpedicao"].IsNullOrEmptyBoolean() ? (DateTime?)null : form["DataExpedicao"].ToDate(),
+            //    IDTipoCliente = form["TipoCliente"].IsNullOrEmptyBoolean() ? (Int32?)null : form["TipoCliente"].ToInt32(),
+            //    Telefone = form["Telefone"].IsNullOrEmptyBoolean() ? null : form["Telefone"].Trim(),
+            //    OrgaoExpeditor = form["OE"].IsNullOrEmptyBoolean() ? null : form["OE"].Trim(),
+            //    TipoDocumento = form["TipoDocumento"].IsNullOrEmptyBoolean() ? (Int32?)null : form["TipoDocumento"].ToInt32(),
+            //    Email = form["Email"].IsNullOrEmptyBoolean() ? null : form["Email"].Trim(),
+            //    Observacao = form["Observacao"].IsNullOrEmptyBoolean() ? null : form["Observacao"].Trim(),
+            //    CPF = form["CPF"].IsNullOrEmptyBoolean() ? null : form["CPF"].Trim(),
+            //    CNPJ = form["CNPJ"].IsNullOrEmptyBoolean() ? null : form["CNPJ"].Trim(),
+            //    RG = form["RG"].IsNullOrEmptyBoolean() ? null : form["RG"].Trim(),
+            //    InscricaoEstadual = form["InscricaoEstadual"].IsNullOrEmptyBoolean() ? null : form["InscricaoEstadual"].Trim(),
+            //    DataNascimento = form["DataNascimento"].IsNullOrEmptyBoolean() ? (DateTime?)null : form["DataNascimento"].ToDate(),
+            //    DataAbertura = form["DataAbertura"].IsNullOrEmptyBoolean() ? (DateTime?)null : form["DataAbertura"].ToDate(),
+            //    Cidadania = form["Cidadania"].IsNullOrEmptyBoolean() ? null : form["Cidadania"].Trim(),
+            //    EstadoCivil = form["EstadoCivil"].IsNullOrEmptyBoolean() ? null : form["EstadoCivil"].Trim(),
+            //    FaixaSalarial = form["faixaSalarial"].IsNullOrEmptyBoolean() ? null : form["faixaSalarial"].Trim(),
+            //    Profissao = form["Profissao"].IsNullOrEmptyBoolean() ? null : form["Profissao"].Trim()
+            //};
+
+            //int enderecoTotal = form["enderecoTotal"].ToInt32();
+            //unidadeConsumidora.Enderecos = new List<EnderecoCliente>();
+            //for (int i = 1; i <= enderecoTotal; i++)
+            //{
+            //    cliente.Enderecos.Add(new EnderecoCliente
+            //    {
+            //        IDCliente = form["ID"].IsNullOrEmptyBoolean() ? (Int32?)null : form["ID"].ToInt32(),
+            //        IDCidade = form["Cidade"].IsNullOrEmptyBoolean() ? (Int32?)null : form["Cidade"].ToInt32(),
+            //        IDEstado = form["Estado"].IsNullOrEmptyBoolean() ? (Int32?)null : form["Estado"].ToInt32(),
+            //        Rua = form["Rua"].IsNullOrEmptyBoolean() ? null : form["Rua"].Trim(),
+            //        Numero = form["Numero"].IsNullOrEmptyBoolean() ? null : form["Numero"].Trim(),
+            //        Bairro = form["Bairro"].IsNullOrEmptyBoolean() ? null : form["Bairro"].Trim(),
+            //        Complemento = form["Complemento"].IsNullOrEmptyBoolean() ? null : form["Complemento"].Trim(),
+            //        CEP = form["CEP"].IsNullOrEmptyBoolean() ? null : form["CEP"].Trim(),
+            //        LatitudeDecimal = form["LatitudeDecimal"].IsNullOrEmptyBoolean() ? (Decimal?)null : form["LatitudeDecimal"].ToDecimal(),
+            //        LongitudeDecimal = form["LongitudeDecimal"].IsNullOrEmptyBoolean() ? (Decimal?)null : form["LongitudeDecimal"].ToDecimal(),
+            //        Padrao = form["Padrao"].ToBoolean(),
+            //        Ativo = form["Ativo"].ToBoolean(),
+            //        Observacao = form["EnderecoObs"].IsNullOrEmptyBoolean() ? null : form["EnderecoObs"].Trim(),
+            //        LatitudeHoras = form["LatitudeHoras"].IsNullOrEmptyBoolean() ? null : form["LatitudeHoras"].Trim(),
+            //        LongitudeHoras = form["LongitudeHoras"].IsNullOrEmptyBoolean() ? null : form["LongitudeHoras"].Trim(),
+            //        LocalInstalacao = form["enderecoInstalacao"].ToBoolean()
+            //    });
+            //}
+
+            //int? resultado = new ClienteBusiness().InsertCliente(cliente);
+
+            //dynamic obj = new System.Dynamic.ExpandoObject();
+            //if (resultado == 1)
+            //{
+            //    obj.Resultado = true;
+            //    obj.Mensagem = "Registro salvo com sucesso!";
+            //}
+            //else
+            //{
+            //    obj.Resultado = false;
+            //    obj.Mensagem = "Falha ao realizar cadastro!";
+            //}
+            //TempData["Resultado"] = obj;
+
+            return RedirectToAction("ClienteEndereco");
+        }
     }
 }
 
