96545381551859124967cd158d70199734402976
Leandro Valle <leandro_lefera@hotmail.com>
2023-01-31 10:56:35 -0300

    31/01/2023

--- GHSContratoWeb/Controllers/ContratoController.cs
+++ GHSContratoWeb/Controllers/ContratoController.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Stimulsoft.Report;
+using Stimulsoft.Report.Mvc;
+using GHSContratoWeb.Models.Business;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GHSContratoWeb.Models.Mapping;
 
 namespace GHSContratoWeb.Controllers
 {
@@ -12,6 +16,20 @@ namespace GHSContratoWeb.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult GetReport(int id = 1)
+        {
+            // Create the report object
+            var report = new StiReport();
+            List<EnderecoCliente> list = new EnderecoClienteBusiness().SelectEnderecoCliente();
+            report.Load(@"C:\Users\Usuario\source\repos\GHSContratoWeb\GHSContratoWeb\Reports\Pais.mrt");
+            report.RegBusinessObject("lista", list);
+            return StiMvcViewer.GetReportResult(report);
+        }
+        public ActionResult ViewerEvent()
+        {
+            return StiMvcViewer.ViewerEventResult();
         }
     }
 }
